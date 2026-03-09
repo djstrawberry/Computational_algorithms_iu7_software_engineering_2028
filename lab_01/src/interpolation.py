bc70b9f81599b1784a072199b51528735d74c4eb
djstrawberry <djstrawbrerry@gmail.com>
2026-03-09 18:28:44 +0300

    finished 1D interpolation (+ some tests).

--- lab_01/src/interpolation.py
+++ lab_01/src/interpolation.py
@@ -1,10 +1,41 @@
 from .config import INTERPOLATION_DEGREES
 import bisect
 
+def calculate_Newton_polynomial(x: float, x_args: list, coefficients: list) -> float:
+
+    polynomial_value = 0
+    product = 1
+
+    for i in range(len(coefficients)):
+        polynomial_value += coefficients[i] * product
+        product *= x - x_args[i]
+
+    return polynomial_value
+
 def interpolate_by_1D_Newton(x: float, x_args: list, y_args: list, degree: int) -> float:
 
     nodes_quantity = degree + 1
     nodes = get_nearest_nodes(x, x_args, nodes_quantity)
+    x_local = [x_args[i] for i in nodes]
+    y_local = [y_args[i] for i in nodes]
+    divided_differences = compute_divided_differences(x_local, y_local)
+    return calculate_Newton_polynomial(x, x_local, divided_differences)
+
+
+def compute_divided_differences(x_args: list, y_args: list) -> list:
+
+    if (len(x_args) != len(y_args)):
+        raise ValueError("Количество аргументов x и y должно быть одинаковым.")
+    
+    divided_differences = y_args.copy()
+    args_length = len(x_args)
+
+    for i in range(args_length):
+        for j in range(args_length - 1, i, -1):
+            divided_differences[j] = (divided_differences[j] - divided_differences[j - 1]) / (x_args[j] - x_args[j - i - 1])
+    
+    return divided_differences
+
 
 def get_nearest_nodes(x: float, x_args: list, nodes_quantity: int) -> list:
 
@@ -25,7 +56,7 @@ def get_nearest_nodes(x: float, x_args: list, nodes_quantity: int) -> list:
             else:
                 right += 1
                 
-    return x_args[left + 1 : right]
+    return list(range(left + 1, right))
 
         
 def find_x_position(x: float, x_args: list) -> int:
--- lab_01/tests/test_interpolation.py
+++ lab_01/tests/test_interpolation.py
@@ -1,22 +1,51 @@
-from src.interpolation import get_nearest_nodes
+from src.interpolation import get_nearest_nodes, compute_divided_differences, calculate_Newton_polynomial
+EPS = 1e-10
 
+# -----------------------------------
+# Тесты для функции get_nearest_nodes
+# -----------------------------------
 def test_get_nearest_nodes_middle():
     x_values = [0, 2, 4, 6, 8]
     nodes = get_nearest_nodes(3, x_values, 3)
 
     assert len(nodes) == 3
-    assert nodes == [0, 2, 4] or nodes == [2, 4, 6]
+    assert nodes == [0, 1, 2] or nodes == [1, 2, 3]
 
 
 def test_get_nearest_nodes_left_edge():
     x_values = [0, 2, 4, 6, 8]
     nodes = get_nearest_nodes(-1, x_values, 3)
 
-    assert nodes == [0, 2, 4]
+    assert nodes == [0, 1, 2]
 
 
 def test_get_nearest_nodes_right_edge():
     x_values = [0, 2, 4, 6, 8]
     nodes = get_nearest_nodes(10, x_values, 3)
 
-    assert nodes == [4, 6, 8]
+    assert nodes == [2, 3, 4]
+
+# ---------------------------------------------
+# Тесты для функции compute_divided_differences
+# ---------------------------------------------
+def test_compute_divided_differences():
+    x_args = [1, 2, 3]
+    y_args = [1, 4, 9]
+
+    divided_differences = compute_divided_differences(x_args, y_args)
+
+    assert len(divided_differences) == len(x_args)
+    assert divided_differences[0] == 1
+    assert divided_differences[1] == 3
+    assert divided_differences[2] == 1
+
+# ---------------------------------------------
+# Тесты для функции calculate_Newton_polynomial
+# ---------------------------------------------
+def test_calculate_Newton_polynomial():
+    x_args = [1, 2, 3]
+    coefficients = [1, 3, 1]
+
+    polynomial_value = calculate_Newton_polynomial(2.5, x_args, coefficients)
+
+    assert abs(polynomial_value - 6.25) < EPS
